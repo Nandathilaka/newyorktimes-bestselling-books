62cc1599a1c5ecdfe15b8e2e550217e27d94f7f0
nandathilaka <thilinachamikashtc@gmail.com>
2020-12-01 13:12:35 +0530

    Submit React Application
    Best selling book list React Application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NewYorkBestSellingBooksApp from './component/NewYorkBestSellingBooksApp';
+
+function App() {
+return (
+    <div className="container">
+        <NewYorkBestSellingBooksApp />
+      </div>
+  );
+}
+
+export default App;
